75215bb8ce8bc53b2a8f9c91b48bbade3a4a1dd1
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2024-09-21 18:39:02 -0300

    Trabalho feito

--- ColheitaFeliz/src/main/java/com/mycompany/colheitafeliz/ColheitaFeliz.java
+++ ColheitaFeliz/src/main/java/com/mycompany/colheitafeliz/ColheitaFeliz.java
@@ -4,6 +4,16 @@ package com.mycompany.colheitafeliz;
 public class ColheitaFeliz {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Fazenda fazenda = new Fazenda();
+        
+        Milho milho = new Milho();
+        Batata batata = new Batata();
+        Tomate tomate = new Tomate();
+        
+        for(int i = 0; i < 6; i++){
+            milho.Crescer();
+            batata.Crescer();
+            tomate.Crescer();
+        }
     }
 }
--- ColheitaFeliz/src/main/java/com/mycompany/colheitafeliz/Fazenda.java
+++ ColheitaFeliz/src/main/java/com/mycompany/colheitafeliz/Fazenda.java
@@ -3,7 +3,40 @@ package com.mycompany.colheitafeliz;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Fazenda /*extends classe 1, classe 2*/{
+public class Fazenda {
     private List<Cultura> culturas;
+    private List<Animal> animais;
+    private int AguaDisponivel;
+    private int FertilizantesDisponiveis;
+    private int RacaoDisponivel;
+    private int Moedas;
     
+    public Fazenda(){
+        culturas = new ArrayList<>();
+        animais = new ArrayList<>();
+        AguaDisponivel = 500;
+        FertilizantesDisponiveis = 300;
+        RacaoDisponivel = 1000;
+        Moedas = 1500;
+    }
+    
+    public void PlantarCultura(Cultura cultura){
+        culturas.add(cultura);
+    }
+    
+    public void ColherCultura(Cultura cultura){
+    
+    }
+    
+    public void AdicionalAnimal(Animal animal){
+        animais.add(animal);
+    }
+    
+    public void AlimentarAnimal(Animal animal){
+        if(RacaoDisponivel >= animal.ConsumoDeRacao){
+            RacaoDisponivel -= animal.ConsumoDeRacao;
+            animal.Alimentar();
+            System.out.println("Animal alimentado");
+        }
+    }
 }
