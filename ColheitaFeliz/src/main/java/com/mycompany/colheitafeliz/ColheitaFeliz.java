d2abec951aa93ac196f4e7e481a696d9ff5f5abd
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2024-09-27 00:34:07 -0300

    Finalizado real

--- ColheitaFeliz/src/main/java/com/mycompany/colheitafeliz/ColheitaFeliz.java
+++ ColheitaFeliz/src/main/java/com/mycompany/colheitafeliz/ColheitaFeliz.java
@@ -9,11 +9,31 @@ public class ColheitaFeliz {
         Milho milho = new Milho();
         Batata batata = new Batata();
         Tomate tomate = new Tomate();
+        fazenda.PlantarCultura(milho);
+        fazenda.PlantarCultura(batata);
+        fazenda.PlantarCultura(tomate);
         
         for(int i = 0; i < 6; i++){
             milho.Crescer();
             batata.Crescer();
             tomate.Crescer();
         }
+        
+        fazenda.ColherCultura(milho);
+        fazenda.ColherCultura(batata);
+        fazenda.ColherCultura(tomate);
+        
+        Galinha galinha = new Galinha();
+        Ovelha ovelha = new Ovelha();
+        Vaca vaca = new Vaca();
+        fazenda.AlimentarAnimal(galinha);
+        fazenda.AlimentarAnimal(ovelha);
+        fazenda.AlimentarAnimal(vaca);
+        
+        fazenda.ColetarProducao(galinha);
+        fazenda.ColetarProducao(ovelha);
+        fazenda.ColetarProducao(vaca);
+        
+        System.out.println("Moedas totais" + fazenda.getMoedas());
     }
 }
